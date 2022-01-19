2dd134da4376d4a7f5d4254d73d07aa60a690f93
kajal kumari <49594530+Kajalkumari677@users.noreply.github.com>
2022-01-19 12:06:27 +0530

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,17 @@
+function navbar() {
+    return `<div id="navbar">
+       <h3><a href="index.html">Home</a></h3>
+   
+
+    <div id="option">
+
+       <h3><a href="electronic.html">Electronic</a></h3>
+       <h3><a href="jewellery.html">Jewellery</a></h3>
+       <h3><a href="">Signin</a></h3>
+       <h3><a href="">Signup</a></h3>
+
+    </div>
+     </div>`
+}
+
+export default navbar//default name(navbar) we can import any name 
